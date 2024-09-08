f1b20326f0c1e5fa869ae07c6812edfcab5b6fa0
nathansc0de <dipartanatas@gmail.com>
2024-09-08 01:36:05 +0100

    created the array of bars

--- src/App.js
+++ src/App.js
@@ -1,22 +1,43 @@
-import './App.css';
 import React, { useState } from 'react';
 
 function App() {
-  let size = 500;
-  let speed = 50; 
-
+  
   //state to hold value of the size of the array when moving the slider
-  const [sizeValue, setSizeValue] = useState(500);
+  const [sizeValue, setSizeValue] = useState(100);
 
   //state to hold value of the speed of the sort when moving the slider
   const [speedValue, setSpeedValue] = useState(50);
 
+  //state to hold the current array
+  const [currentArray, setCurrentArray] = useState(newArray(sizeValue));
+
   //update the state when the sliders change
   const handleSizeChange = (event) => {
     setSizeValue(event.target.value);
+    setCurrentArray(newArray(event.target.value));
   }
   const handleSpeedChange = (event) => {
     setSpeedValue(event.target.value);
+  }
+
+  //creates new array based on size passed in
+  function newArray(size) {
+    const array = [];
+    for(let i = 0; i < size; i++) {
+      array.push(randomInt(1, 700)) //random height between 1 and 700 pixels
+    }
+    
+    return array;
+  }
+
+  //re-randomizes the array
+  const randomizeArray = () => {
+    setCurrentArray(newArray(sizeValue));
+  }
+
+  //returns random int between min and max
+  function randomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
   }
 
   return (
@@ -25,13 +46,13 @@ function App() {
         <div className="menu">
           <h1>Sorting Visualizer</h1>
           <div className="options">
-          <button>Shuffle Array</button>
-          <button>Sort</button>
+            <button onClick={randomizeArray}>Randomize Array</button>
+            <button>Sort</button>
 
             <div className="slider1">
               {'size: ' + sizeValue}
-              <input type="range" className="slider" min="1" 
-              max="1000" value={sizeValue} onChange={handleSizeChange} id="arraySize"/>
+              <input type="range" className="slider" min="10" 
+              max="200" value={sizeValue} onChange={handleSizeChange} id="arraySize"/>
             </div>
             <div className="slider2">
             {'speed: ' + speedValue}
@@ -39,14 +60,30 @@ function App() {
               max="100" value={speedValue} onChange={handleSpeedChange} id="sortSpeed"/>
             </div>
 
+            {/* select type of sort */}
             <button>Selection Sort</button>
             <button>Bubble Sort</button>
             <button>Insertion Sort</button>
             <button>Merge Sort</button>
             <button>Quick Sort</button>
           </div>
-          <div className="visual">
+        </div>
 
+        {/* Visualization area */}
+        <div className="visual">
+          <div className="array-bars">
+            {currentArray.map((value, idx) => (
+              <div 
+              className="array-bar"
+              key={idx}
+              style={{
+                height: `${value}px`,
+                width: `${1800/sizeValue}px`,
+                marginLeft: `${100/sizeValue}px`,
+                marginRight: `${100/sizeValue}px`
+              }}
+              ></div>
+            ))}
           </div>
         </div>
       </div>
